2f0803a1fbaf9298ec79fa7d938038a33bcabc83
chintanpatel <chintanpatel9065@gmail.com>
2025-07-27 10:17:31 +0530

    Add `StatusRepository` with a method to search statuses by type

---
+++ src/main/java/org/chintanpatel/pms/status/StatusRepository.java
@@ -0,0 +1,12 @@
+package org.chintanpatel.pms.status;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository( "statusRepository")
+public interface StatusRepository extends JpaRepository<Status, Long> {
+
+  List<Status> findByStatusTypeContainingIgnoreCase(String statusType);
+}
