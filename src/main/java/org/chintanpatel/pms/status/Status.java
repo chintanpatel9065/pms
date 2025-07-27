946adced9bc5c56661ddd3e61b29ddc5ab362bdc
chintanpatel <chintanpatel9065@gmail.com>
2025-07-27 10:16:02 +0530

    Add a ` Status ` entity with necessary fields, relationships, and validation annotations

---
+++ src/main/java/org/chintanpatel/pms/status/Status.java
@@ -0,0 +1,57 @@
+package org.chintanpatel.pms.status;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import org.chintanpatel.pms.task.Task;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "status")
+public class Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "status_id", nullable = false)
+    private Long statusId;
+
+    @NotEmpty(message = "Please Provide Status Type")
+    @Column(name = "status_type", nullable = false)
+    private String statusType;
+    private Set<Task> tasks = new HashSet<>();
+
+    public Status() {
+    }
+
+    public Status(Long statusId, String statusType, Set<Task> tasks) {
+        this.statusId = statusId;
+        this.statusType = statusType;
+        this.tasks = tasks;
+    }
+
+    public Long getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(Long statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getStatusType() {
+        return statusType;
+    }
+
+    public void setStatusType(String statusType) {
+        this.statusType = statusType;
+    }
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
